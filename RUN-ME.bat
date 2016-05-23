83ed895f8a084319cf0251ea703f571512fac772
Blake Bartenbach <Blake Bartenbach>
2016-05-22 21:14:12 -0500

    * Updated to a better ExecutionPolicy method

--- RUN-ME.bat
+++ RUN-ME.bat
@@ -1,6 +1,4 @@
 @echo off
 mode con:cols=90 lines=50
 
-powershell Set-ExecutionPolicy Unrestricted
-powershell .\powershell.ps1
-powershell Set-ExecutionPolicy Default
+powershell -ExecutionPolicy Bypass .\powershell.ps1
